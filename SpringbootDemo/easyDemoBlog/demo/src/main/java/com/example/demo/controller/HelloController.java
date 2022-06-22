d344fbe7869717815495493d753f0d93713c8570
Stone <19321123@hdu.edu.cn>
2022-06-23 01:08:22 +0800

    更新demo工程
    新效果可以在本地网页展示IP地址+日期

--- SpringbootDemo/easyDemoBlog/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ SpringbootDemo/easyDemoBlog/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,11 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author stone
  */
@@ -11,9 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/helloDemo")
     public String hello() {
-        System.out.println("Catch request");
-        return "Easy demo for running jar via docker in server.";
+        InetAddress addr = null;
+        String ip = null;
+        String address = null;
+        boolean getAddr = true;
+        try {
+            addr = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            getAddr = false;
+            ip = "127.0.0.1";
+            address = "Stone";
+        }
+        if (getAddr) {
+            ip=addr.getHostAddress();
+            address=addr.getHostName();
+        }
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateNowStr = sdf.format(d);
+        if (getAddr) {
+            return "IP: " + ip + "\n Host name: " + address + "\n Time: " + dateNowStr;
+        } else {
+            return "Error when catching your host.\nDefault ip: " + ip + "\n Default host name: "
+                    + address + "\n Time: " + dateNowStr;
+        }
     }
 }
